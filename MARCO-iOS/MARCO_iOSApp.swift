95107fa553590cddb9cef7ff2d7ba69a25f9882b
josecas <jmcg0901@gmail.com>
2022-10-12 12:38:22 -0500

    Initial Commit

---
+++ MARCO-iOS/MARCO_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MARCO_iOSApp.swift
+//  MARCO-iOS
+//
+//  Created by Jose Castillo on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MARCO_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
