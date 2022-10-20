b4d39587255a22d2545ee3e8a5b53fe4c9c739bf
josecas <jmcg0901@gmail.com>
2022-10-20 14:12:40 -0500

    Developed TutorialView

--- MARCO-iOS/Views/ContentView.swift
+++ MARCO-iOS/Views/ContentView.swift
@@ -6,42 +6,14 @@
 //
 
 import SwiftUI
-import SceneKit
-import ARKit
 
 struct ContentView: View {
     
-    @State private var selection = 2
-    
     var body: some View {
-        
-        TabView(selection:$selection) {
-            Text("Your Progress")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "list.clipboard")
-                    Text("Progress")
-                }
-                .tag(1)
-
-            ARView()
-                .edgesIgnoringSafeArea(.top)
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "camera.fill")
-                    Text("Camera")
-                }
-                .tag(2)
-
-            Text("Settings")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "gear")
-                    Text("Settings")
-                }
-                .tag(3)
+        ZStack {
+            TabBarView()
+            TutorialView()
         }
-        
     }
 }
 
